dfdde4bb7489dc43db342ea0f70734a330bf9931
amanda-santiago <ampass@cin.ufpe.br>
2021-08-20 13:07:28 -0300

    Criação e Alteração dos testes de cadastro de monitor

--- tests-acceptance/features/monitores.feature
+++ tests-acceptance/features/monitores.feature
@@ -3,7 +3,13 @@ I want to: Cadastrar e gerenciar os monitores das minhas turmas
 So that: Possa manter coerente a organização dos meus monitores, afim de realizar e acompanhar alocações e seus trabalhos.
 
 Scenario: Cadastro de monitor com sucesso
-Given estou na página de cadastro de monitor
-Given não vejo o monitor com CPF "14323456700" na lista de monitores
+Given estou na pagina de cadastro de monitor
+Given nao vejo o monitor com CPF "14323456701" na lista de monitores
+When tento cadastrar o monitor "Gislayne Vitorino" com CPF "14323456701"
+Then vejo "Gislayne Vitorino" com CPF "14323456701" na lista de monitores 
+
+Scenario: Cadastro de monitor com CPF existente
+Given estou na pagina de cadastro de monitor
+Given vejo o monitor "Bruno Martins" com CPF "14323456700"
 When tento cadastrar o monitor "Lucas Silva" com CPF "14323456700"
-Then vejo "Lucas Silva" com CPF "14323456700" na lista de monitores
+Then nao vejo o monitor "Lucas Silva" na lista de monitores
--- tests-acceptance/stepdefinitions/monitores.ts
+++ tests-acceptance/stepdefinitions/monitores.ts
@@ -7,32 +7,45 @@ let expect = chai.expect;
 let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
 
 defineSupportCode(function ({ Given, When, Then }) {
-    Given(/^estou na página de cadastro de monitor$/, async () => {
+    Given(/^estou na pagina de cadastro de monitor$/, async () => {
         await browser.get("http://localhost:4200/monitores");
         await expect(browser.getTitle()).to.eventually.equal('TaGui');
-    })
+    });
 
-    Given(/^não vejo o monitor com CPF "(\d*)" na lista de monitores$/, async (cpf) => {
+    Given(/^nao vejo o monitor com CPF "(\d*)" na lista de monitores$/, async (cpf) => {
         var allcpfs : ElementArrayFinder = element.all(by.name('cpflist'));
         var samecpfs = allcpfs.filter(elem =>
                                       elem.getText().then(text => text === cpf));
         await samecpfs.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
     });
 
+    Given(/^vejo o monitor "([^\"]*)" com CPF "(\d*)"$/, async (name, cpf) => {
+        await $("input[name='namebox']").sendKeys(<string> name);
+        await $("input[name='cpfbox']").sendKeys(<string> cpf);
+        await element(by.buttonText('Adicionar')).click();
+        
+    });
+    
     When(/^tento cadastrar o monitor "([^\"]*)" com CPF "(\d*)"$/, async (name, cpf) => {
         await $("input[name='namebox']").sendKeys(<string> name);
         await $("input[name='cpfbox']").sendKeys(<string> cpf);
         await element(by.buttonText('Adicionar')).click();
     });
-
     Then(/^vejo "([^\"]*)" com CPF "(\d*)" na lista de monitores$/, async (name, cpf) => {
         var allcpfs : ElementArrayFinder = element.all(by.name('cpflist'));
         var allnomes : ElementArrayFinder = element.all(by.name('nomelist'));
         var samecpfs = allcpfs.filter(elem =>elem.getText().then(text => text === cpf));
         var samenomes = allnomes.filter(elem =>elem.getText().then(text => text === name));
 
-        console.log(allcpfs + " " + allnomes + " " + samecpfs + " " + samenomes);
         await allcpfs.filter(elem => pAND(samecpfs,samenomes)).then
                    (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
   });
+
+    Then(/^nao vejo o monitor "([^\"]*)" na lista de monitores$/, async (name) => {
+        var allnomes : ElementArrayFinder = element.all(by.name('nomelist'));
+    var samenomes = allnomes.filter(elem =>
+                                  elem.getText().then(text => text === name));
+    await samenomes.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+  });
+
 });
